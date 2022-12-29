ae7b95203742fd2deda85889dedd78b394af2fec
Cristian <cristianvasquez@gmail.com>
2022-12-29 01:45:24 +0100

    chore: update dependencies

---
+++ example.js
@@ -0,0 +1,22 @@
+import { readFile } from 'fs/promises'
+import { resolve } from 'path'
+import ns from './src/namespaces.js'
+
+import { createTriplifier } from './index.js'
+
+const dir = './test/markdown/'
+
+// Config
+const triplifier = await createTriplifier(dir, {
+  baseNamespace: ns.ex,
+  mapper: {
+    'is a': ns.rdf.type, 'are': ns.rdf.type, 'ex:knows': ns.ex.knows,
+  },
+})
+
+for (const file of triplifier.index.files.filter(x => x.endsWith('.md'))) {
+  console.log('Processing file:', file)
+  const text = await readFile(resolve(dir, file), 'utf8')
+  const pointer = triplifier.toRdf(text, { path: file })
+  console.log(pointer.dataset.toString())
+}
--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+import { buildIndex } from './src/indexers/buildIndex.js'
+import ns from './src/namespaces.js'
+import { createTermMapper } from './src/termMapper/defaultUriResolver.js'
 import { toRdf } from './src/toRdf.js'
 
-export { toRdf }
+const defaultOptions = {
+  mappers: {}, baseNamespace: ns.ex.vault,
+}
+
+async function createTriplifier (dir, options = {}) {
+  const index = await buildIndex(dir)
+  const _options = { ...defaultOptions, index, ...options }
+  const termMapper = createTermMapper(_options)
+  return {
+    toRdf: (text, options) => toRdf(text, { termMapper, ...options }),
+    index,
+  }
+
+}
+
+export { toRdf, createTriplifier }
 
